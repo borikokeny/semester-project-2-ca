1fa0606156882e2648eaf0afce47af32c6d8c24c
Bori Kokeny <bori@minerali.hu>
2024-01-31 20:55:39 +0100

    Changes listing html page

--- src/js/templates/listings.mjs
+++ src/js/templates/listings.mjs
@@ -1,78 +1,84 @@
-import { viewListings } from "../api/listing/view.mjs";
-import { renderListingCardTemplates } from "./listingCard.mjs";
+// import { viewListings } from "../api/listing/view.mjs";
+// import { renderListingCardTemplates } from "./listingCard.mjs";
  
-export async function renderListingTemplateNEW() {
-  const listingContainer = document.querySelector('#listing');
+// export async function renderListingTemplateNEW() {
+//   const listingContainer = document.querySelector('#listing');
 
-  const renderListingsNEW = async (listings) => {
-    try {
-      renderListingCardTemplates(listings, listingContainer);
-    } catch (error) {
-      return(error, listingContainer)
-    }
-  }
-};
+//   const renderListingsNEW = async (listings) => {
+//     try {
+//       renderListingCardTemplates(listings, listingContainer);
+//     } catch (error) {
+//       return(error, listingContainer)
+//     }
+//   }
+// };
 
-try {
-  const listings = await viewListings();
-  renderListingsNEW(listings);
-} catch (error) {
-  return(error, listingContainer)
+// try {
+//   const listings = await viewListings();
+//   renderListingsNEW(listings);
+// } catch (error) {
+//   return(error, listingContainer)
+// }
+
+
+
+
+
+
+
+
+
+
+
+import { viewListings } from "../api/listing/view.mjs";
+
+export function listingTemplateA(postData) {
+  return `<div class="listing card" id=${postData.id}>${postData.title}</div>`
 }
 
+export function listingTemplateB(postData) {
+  const listing = document.createElement("div");
 
-
-
-
-
-
-
-
-
-
-// // import { viewListings } from "../api/listing/view.mjs";
-
-// // export function listingTemplateA(postData) {
-// //   return `<div class="listing card" id=${postData.id}>${postData.title}</div>`
-// // }
-
-// // export function listingTemplateB(postData) {
-// //   const listing = document.createElement("div");
-// //   listing.classList.add("listing"),
-// //   listing.innerText = postData.title;
-// //   // const listing = document.createElement("div");
-// //   // listing.classList.add("listing");
-// //   // listing.innerText = postData.title;
   
-// //   // if(postData.description) {
-// //   //   const description = document.createElement('p');
-// //   //   description.innerText = postData.description;
-// //   //   listing.append(description)
-// //   // }
+  listing.classList.add("listing", "col"),
 
-// //   // if(postData.endsAt) {
-// //   //   const endsAt = document.createElement('p');
-// //   //   endsAt.innerText = postData.endsAt;
-// //   //   listing.append(endsAt)
-// //   // }
+  
+  listing.innerText = postData.title;
+  // const listing = document.createElement("div");
+  // listing.classList.add("listing");
+  // listing.innerText = postData.title;
 
-// //   // if(postData.media) {
-// //   //   const img = document.createElement('img');
-// //   //   img.src = postData.media;
-// //   //   img.alt = postData.title;
-// //   //   listing.append(img)
-// //   // }
-// //   return listing;
-// // }
 
-// // // export function renderListingTemplate(postData, parent) {
-// // //   // parent.innerHTML = listingTemplate(postData)
+  if(postData.media) {
+    const img = document.createElement('img');
+    img.src = postData.media;
+    img.alt = postData.title;
+    listing.append(img)
+  }
 
-// // //   parent.append(listingTemplateB(postData))
-// // //   //ez a kod teszi ki az egy listinget egy html oldalra
-// // // }
+  if(postData.description) {
+    const description = document.createElement('p');
+    description.innerText = postData.description;
+    listing.append(description)
+  }
 
-// // export function renderListingTemplates(postDataList, parent) {
-// //   parent.append(...postDataList.map(listingTemplateB))
-// //   //ez a kod rakja ki a listings/index.html oldalra a listingeket
-// // }
+  if(postData.endsAt) {
+    const endsAt = document.createElement('p');
+    endsAt.innerText = postData.endsAt;
+    listing.append(endsAt)
+  }
+
+  return listing;
+}
+
+// export function renderListingTemplate(postData, parent) {
+//   // parent.innerHTML = listingTemplate(postData)
+
+//   parent.append(listingTemplateB(postData))
+//   //ez a kod teszi ki az egy listinget egy html oldalra
+// }
+
+export function renderListingTemplates(postDataList, parent) {
+  parent.append(...postDataList.map(listingTemplateB))
+  //ez a kod rakja ki a listings/index.html oldalra a listingeket
+}
