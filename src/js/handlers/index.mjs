810d347f72f00e1e780a3b63fc92464d24d8a530
Bori Kokeny <bori@minerali.hu>
2024-02-04 14:22:49 +0100

    Updates handlers index.mjs

--- src/js/handlers/index.mjs
+++ src/js/handlers/index.mjs
@@ -4,4 +4,5 @@ export * from "./updateProfile.mjs";
 export * from "./logout.mjs";
 export * from "./createListing.mjs";
 export * from "./updateListing.mjs";
+export * from "./bidListing.mjs";
 export * from "./search.mjs";
